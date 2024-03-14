df4535f4bf5e96a075c7ad9e0048d59ac0b1a700
Julian Coward <juliancoward54@gmail.com>
2024-03-14 17:21:19 -0500

    Added tool files

--- dream-team/src/pages/ToolsPage.js
+++ dream-team/src/pages/ToolsPage.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from 'react'; 
+import '../styling/Tools.css'; 
+import profileIcon from '../images/profile_icon.png';
+import settingsIcon from '../images/settings_icon.png';
+import { Link } from 'react-router-dom';
 
 const ToolsPage = () => {
-  return <div><h1>Tools</h1><p>This is the Tools page.</p></div>;
-};
+  return <div>
+    <div className = "tools-container">    
+      <header className="home-header">
+        <div className="right-icons">
+            <Link to="/profile"><img src={profileIcon} alt="Profile" /></Link>
+            <Link to="/settings"><img src={settingsIcon} alt="Settings" /></Link>
+        </div>
+      </header>
 
+      <ul className = "flex-list">
+      <li><label for = "set-alarms">Set Alarms</label></li>
+      <li><label for = "calming-sounds">Calming Sounds</label></li>
+      <li><label for = "audio-books">Audio Books</label></li>
+      </ul>
+    </div>
+
+    </div>;
+};
+/* links to Alarms, calming sounds, audio books, meditation */
 export default ToolsPage;
