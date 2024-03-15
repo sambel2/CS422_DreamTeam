b88b99e375f91fcdf032bba00181190397db7bd7
Julian Coward <juliancoward54@gmail.com>
2024-03-14 21:32:25 -0500

    Adding nav to tools, still fixing image sizes

--- dream-team/src/pages/ToolsPage.js
+++ dream-team/src/pages/ToolsPage.js
@@ -2,21 +2,24 @@ import React from 'react';
 import '../styling/Tools.css'; 
 import profileIcon from '../images/profile_icon.png';
 import settingsIcon from '../images/settings_icon.png';
+
+import playButton from '../images/play_button.png';
+import fastFoward from '../images/fast_forward.png';
+import rewindButton from '../images/rewind-button.png';
+
 import { Link } from 'react-router-dom';
 
 const ToolsPage = () => {
   return <div>
-    
-    <div className = "tools-container">    
-       <header className="tools-header">
+    <header className="tools-header">
         <div></div>
         <div className="right-icons">
             <Link to="/profile"><img src={profileIcon} alt="Profile" /></Link>
             <Link to="/settings"><img src={settingsIcon} alt="Settings" /></Link>
         </div>
       </header>
+    <div className = "tools-container">    
           <h1>Tools</h1>
-    <div class="flex-list">
       <div class="box">
         <h2>Alarms</h2>
         <label for="times">Times:</label>
@@ -25,27 +28,30 @@ const ToolsPage = () => {
         <option value="2am">7 am</option>
         <option value="3am">8 am</option>
         </select>
-      </div>
     </div>
       
     <div class="box">
       <h2>Calming Sounds</h2>
-      <img src="play_button.png" alt="Play Button" />
-      <img src="rewind_button.png" alt="Rewind Button" />
-      <img src="fast_forward_button.png" alt="Fast Forward Button" />
+      <img src={playButton} alt="Play Button"/>
+        <img src={rewindButton} alt="Rewind Button"/>
+        <img src={fastFoward} alt="Fast Forward Button"/>
     </div>
 
       <div class="box">
         <h2>Audio Books</h2>
-        {/* <img src="play_button.png" alt="Play Button">
-        <img src="rewind_button.png" alt="Rewind Button">
-        <img src="fast_forward_button.png" alt="Fast Forward Button"> */}
+        <img src={playButton} alt="Play Button"/>
+        <img src={rewindButton} alt="Rewind Button"/>
+        <img src={fastFoward} alt="Fast Forward Button"/>
       </div>
       <div class="box">
         <h2>Meditation</h2>
       </div>
     </div>
-
+    <nav className="bottom-nav">
+        <Link to="/tools">Tools</Link>
+        <Link to="/tracking">Tracking</Link>
+        <Link to="/resources">Resources</Link>
+      </nav>
     </div>;
 };
 /* links to Alarms, calming sounds, audio books, meditation */
