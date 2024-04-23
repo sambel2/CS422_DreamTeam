21ddc1525d619d4a9be9b3a20579f82327c69ef4
Julian Coward <juliancoward54@gmail.com>
2024-04-22 19:39:56 -0500

    Editing the top buttons

--- dream-team/src/pages/SettingsPage.js
+++ dream-team/src/pages/SettingsPage.js
@@ -8,28 +8,25 @@ import SettingsTitle from '../images/Settings_Title.png';
 
 const YourComponent = () => {
   return (
-    <div>
-      <header className="settings-header">
-        <div className="left-icons">
-          <Link to="/home"><img src={homeIcon} alt="Home"/></Link>
-        </div>
-        <div className="right-icons">
-          <Link to="/profile"><img src={profileIcon} alt="Profile" /></Link>
-        </div>
-      </header>
-
-      <div className="settings-container">
-        <img src={SettingsTitle} alt="SettingsTitle" />
-        <div className="settings-box">Account</div>
-        <div className="settings-box">Notifications</div>
-        <div className="settings-box">Language</div>
-        <div className="settings-box">Accessibility</div>
-        <div className="settings-box">Support</div>
-        <div className="settings-box">Terms & Conditions</div>
-        <div className="settings-box">Privacy Policy</div>
-        <div className="settings-box">Log out</div>
+  <div className="settings-container">
+    <header className="settings-header">
+      <div className="left-icons">
+      <Link to="/home"><img src={homeIcon} alt="Home"/></Link>
       </div>
-      <BottomNav/>
+      <div className="right-icons">
+      <Link to="/profile"><img src={profileIcon} alt="Profile" /></Link>
+      </div>
+    </header>
+    <img src={SettingsTitle} alt="SettingsTitle" />
+    <div className="settings-box">Account</div>
+    <div className="settings-box">Notifications</div>
+    <div className="settings-box">Language</div>
+    <div className="settings-box">Accessibility</div>
+    <div className="settings-box">Support</div>
+    <div className="settings-box">Terms & Conditions</div>
+    <div className="settings-box">Privacy Policy</div>
+    <div className="settings-box">Log out</div>
+    <BottomNav/>
     </div>
   );
 };
