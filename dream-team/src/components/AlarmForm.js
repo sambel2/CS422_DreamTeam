cb716521de7f5ca2375ff5d60728b0b23e5b40e0
Parrama <91933748+Parrama@users.noreply.github.com>
2024-04-24 21:50:17 -0500

    Updated AlarmForm.js

--- dream-team/src/components/AlarmForm.js
+++ dream-team/src/components/AlarmForm.js
@@ -3,43 +3,43 @@ import '../styling/AlarmForm.css';
 
 const AlarmForm = ({ addAlarm }) => {
     // State to store form inputs
-    const [time, setTime] = useState('');
-    const [description, setDescription] = useState('');
+    const [date, setDate] = useState('');
+    const [hours, setHours] = useState('');
 
     // Function to handle form submission
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!time || !description) return; // Prevent adding empty alarms
-        addAlarm({ time, description }); // Pass new alarm data to parent component
-        setTime(''); // Reset form inputs after submission
-        setDescription('');
+        if (!date || !hours) return; // Prevent adding empty alarms
+        addAlarm({ date, hours }); // Pass new alarm data to parent component
+        setDate(''); // Reset form inputs after submission
+        setHours('');
     };
 
     return (
         <div className="alarm-form">
-            <h2>Add New Alarm</h2>
+            <h2>Log Your Sleep Pattern</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label htmlFor="time" class="label-left">Time:</label>
+                    <label htmlFor="date" class="label-left">Date:</label>
                     <input 
                         type="text" 
-                        id="time" 
-                        value={time} 
-                        onChange={(e) => setTime(e.target.value)} 
-                        placeholder="Enter alarm time" 
+                        id="date" 
+                        value={date} 
+                        onChange={(e) => setDate(e.target.value)} 
+                        placeholder="(MM/DD/YYYY)" 
                     />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="description" class="label-left2">Description:</label>
+                    <label htmlFor="hours" class="label-left2">Hours:</label>
                     <input 
                         type="text" 
-                        id="description" 
-                        value={description} 
-                        onChange={(e) => setDescription(e.target.value)} 
-                        placeholder="Enter alarm description" 
+                        id="hours" 
+                        value={hours} 
+                        onChange={(e) => setHours(e.target.value)} 
+                        placeholder="Enter sleep hours" 
                     />
                 </div>
-                <button type="submit" class="alarm-button">Add Alarm</button>
+                <button type="submit" class="alarm-button">Log Hours</button>
             </form>
         </div>
     );
